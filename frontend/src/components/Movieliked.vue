f66501ce9b72a2c0dd933a47d867d28b5bf42b4d
unknown <bendioussoussama@gmail.com>
2021-06-11 15:12:44 +0200

    finish

---
+++ frontend/src/components/Movieliked.vue
@@ -0,0 +1,175 @@
+<template style="background-color: black">
+  <body>
+    <div class="moviecontainer">
+      <div class="container">
+        <img
+          alt="Movie Poster"
+          :src="
+            'https://www.themoviedb.org/t/p/w300_and_h450_bestv2' +
+            movie.poster_path
+          "
+          class="image"
+        />
+        <div class="overlay">
+          <div class="text">{{ movie.overview }}</div>
+        </div>
+      </div>
+
+      <div class="like/dislike">
+        <button @click="like" class="like">
+          +
+          <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+        </button>
+        <button @click="dislike" class="dislike">
+          -
+          <i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+        </button>
+        <br />
+        <button class="learnmore"><b> Add to Watchlist </b></button>
+      </div>
+      <p>
+        <b>
+          {{ movie.title }}
+        </b>
+      </p>
+      <p>
+        {{ movie.release_date ? movie.release_date.slice(0, 4) : undefined }}
+      </p>
+    </div>
+  </body>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "movie",
+  props: {
+    movie: Object,
+  },
+  methods: {
+    like: function () {
+      axios
+        .post("http://localhost:3000/like/like", {
+          movie_id: this.movie.id,
+        })
+        .then(() => {
+          axios.post("http://localhost:3000/score/score", {
+            movie_id: this.movie.id,
+            genre_ids: this.movie.genre_ids,
+          });
+        });
+    },
+    dislike: function () {
+      axios
+        .post("http://localhost:3000/dislike/dislike", {
+          movie_id: this.movie.id,
+        })
+        .then(() => {
+          axios.post("http://localhost:3000/score/scoren", {
+            movie_id: this.movie.id,
+            genre_ids: this.movie.genre_ids,
+          });
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.maincontainer {
+  text-align: center;
+}
+.container {
+  position: relative;
+  width: 100%;
+}
+
+.image {
+  display: block;
+  width: 400;
+  height: 500;
+  position: center;
+}
+.image:hover {
+  position: relative;
+  top: -50px;
+  left: -70px;
+  width: 100%;
+  height: 100%;
+  display: block;
+  z-index: 999;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: 0.7s linear;
+  background-color: black;
+}
+
+.container:hover .overlay {
+  opacity: 0.7;
+}
+
+.text {
+  color: white;
+  font-size: 18px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+/* like/dislike button */
+button.like {
+  width: 30px;
+  height: 30px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  line-heigth: 50px;
+  border-radius: 50%;
+  color: rgba(0, 150, 136, 1);
+  background-color: rgba(38, 166, 154, 0.3);
+  border-color: rgba(0, 150, 136, 1);
+  border-width: 1px;
+  font-size: 17px;
+}
+
+button.dislike {
+  width: 30px;
+  height: 30px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  line-heigth: 50px;
+  border-radius: 50%;
+  color: rgba(255, 82, 82, 1);
+  background-color: rgba(255, 138, 128, 0.3);
+  border-color: rgba(255, 82, 82, 1);
+  border-width: 1px;
+  font-size: 17px;
+}
+button.learnMore {
+  width: 120px;
+  height: 30px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  line-heigth: 50px;
+  color: white;
+  background-color: rgb(228, 187, 8);
+  border-width: 1px;
+  font-size: 12px;
+}
+</style>
--- frontend/src/views/List.vue
+++ frontend/src/views/List.vue
@@ -1,20 +1,18 @@
 <template>
   <h1>My List</h1>
   <h2>Liked</h2>
-  <ul>
-    <li v-for="movie in liked" :key="movie.original_title">
-      <Movie :movie="movie" />
-    </li>
-  </ul>
+  <div class="home">
+    <ul>
+      <li v-for="movie in movies" :key="movie.original_title">
+        <Movie :movie="movie" />
+      </li>
+    </ul>
+  </div>
+
   <h2>Disliked</h2>
-  <ul>
-    <li v-for="movie in disliked" :key="movie.original_title">
-      <Movie :movie="movie" />
-    </li>
-  </ul>
 </template>
 <script>
-import Movie from "@/components/Movie.vue";
+import Movie from "@/components/Movieliked.vue";
 import axios from "axios";
 
 export default {
@@ -25,54 +23,46 @@ export default {
   data: function () {
     return {
       movies: [],
+
       movieName: "",
       movieLoadingError: "",
     };
   },
   methods: {
-    fetchMovies: function (genreName) {
-      let genreID = 21;
-      for (const genre of genres) {
-        if (genre.name == genreName) {
-          genreID = genre.id;
-        }
-      }
-
+    recomm: function () {
       axios
-        .get(
-          "https://api.themoviedb.org/3/discover/movie?api_key=522d421671cf75c2cba341597d86403a&with_genres=" +
-            genreID
-        )
+        .get("http://localhost:3000/like")
         .then((response) => {
-          this.liked = response.data.results;
           console.log(response);
+          for (let i in response.data.movie) {
+            console.log("hiooooo");
+            axios
+              .get(
+                `https://api.themoviedb.org/3/movie/` +
+                  response.data.movie[i].movie_id +
+                  `?api_key=522d421671cf75c2cba341597d86403a`
+              )
+              .then((responses) => {
+                console.log(responses);
+                this.movies = movies.append(responses.data.results);
+                // Do something if call succe
+              })
+              .catch((error) => {
+                this.moviesLoadingError = "An error has occured";
+                console.log(error);
+              });
+          }
         })
-        .catch((error) => {
-          this.moviesLoadingError = "An error has occured";
-          console.log(error);
-        });
-      get(
-        "https://api.themoviedb.org/3/discover/movie?api_key=522d421671cf75c2cba341597d86403a&with_genres=" +
-          genreID
-      )
-        .then((response) => {
-          this.disliked = response.data.results;
-          console.log(response);
-        })
+
         .catch((error) => {
           this.moviesLoadingError = "An error has occured";
           console.log(error);
         });
     },
   },
-  beforeRouteUpdate(to, from, next) {
-    const genreName = to.params.genreName;
-    this.fetchMovies(genreName);
-    next();
-  },
+
   mounted: function () {
-    console.log(this.$route.params.genreName);
-    this.fetchMovies(this.$route.params.genreName);
+    this.recomm();
   },
 };
 </script>
